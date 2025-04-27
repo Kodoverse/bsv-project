b93ab0e74c8e56d14f779e451e5c01db6dff73a0
Emanuele <manu.pogliari@gmail.com>
2025-04-27 19:19:07 +0200

    fixed static data from apis

--- frontend/src/pages/AboutUsPage.vue
+++ frontend/src/pages/AboutUsPage.vue
@@ -1,42 +1,37 @@
 <template>
-    <h1></h1>
-    <h2></h2>
-    <div>
-        <p></p>
-    </div>
+  <h1></h1>
+  <h2></h2>
+  <div>
+    <p></p>
+  </div>
 </template>
 <script>
 import axios from "axios";
-    export default {
-        name: "AboutUsPage",
-        data() {
-            return {
-                aboutSection: [],
-                test: [],
-            }
-        },
-        methods: {
-            getData() {
-                axios.get('http://localhost:8000/api/sections').then((res)=>{ 
-                    this.aboutSection=res.data.results;
-                });   
-                this.filterData();
-            },
-
-            filterData() {
-                this.test = this.aboutSection.filter(element => element.title.trim().toLowerCase() == "aboutus");
-                console.log(this.test);
-           
-            }
-        },
-        mounted() {
-            this.getData();
-            
-        }
-    }
-
-
+export default {
+  name: "AboutUsPage",
+  data() {
+    return {
+      aboutSection: [],
+      test: [],
+    };
+  },
+  methods: {
+    getData() {
+      this.current_page = this.$route.name;
+      axios
+        .get("http://127.0.0.1:8000/api/sections", {
+          params: {
+            title: this.current_page,
+          },
+        })
+        .then((res) => {
+          this.aboutSection = res.data.results;
+        });
+    },
+  },
+  mounted() {
+    this.getData();
+  },
+};
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
