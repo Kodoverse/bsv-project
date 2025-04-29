375a6f166f59bb0ca6baa5d8018fe4804c03efcf
mattia_morriale <mattia.morriale1@gmail.com>
2025-04-29 12:49:59 +0200

    completed all footer layouts and added cdn fontawesome's link

--- frontend/src/components/FooterComponent.vue
+++ frontend/src/components/FooterComponent.vue
@@ -1,6 +1,93 @@
 <template>
-    <footer>
-<h1>Footer</h1>
+    <footer class="bg-white justify-between w-full p-6">
+
+        <div class=" w-1/4 flex flex-col items-center justify-center">
+            <div class="w-full flex justify-center pb-2.5">
+                <h3 class="footer-line text-2xl"><strong>Villaseta</strong></h3>
+            </div>
+
+            <ul class="text-black">
+                <li class="flex pb-1.5">
+                    <div class="flex items-center">
+                        <svg class="w-7 h-7 footer-svg" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.8 13.938h-.011a7 7 0 1 0-11.464.144h-.016l.14.171c.1.127.2.251.3.371L12 21l5.13-6.248c.194-.209.374-.429.54-.659l.13-.155Z"/>
+                        </svg>
+                    </div>
+
+                    <div class="text-sm flex items-center ps-5">
+                        <p>Piazza S. Basilio, 92100 Agrigento AG</p>
+                    </div>
+                </li>
+                <li class="flex">
+                    <div class="flex items-center">
+                        <svg class="w-7 h-7 footer-svg" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                        </svg>
+                    </div>
+
+                    <div class="text-sm flex items-center ps-5">
+                        <p>
+                            Mattina: 9:00 - 14:00 <br>
+                            Pomeriggio: 16:00 - 19:00
+                        </p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+        <div class=" w-1/5 h-full flex flex-col justify-center">
+
+            <div class="w-full flex justify-center py-2.5">
+                <img src="/biblioteca_logo.png" class="h-12" alt="Logo" />
+            </div>
+
+            <div class="flex justify-center w-full">
+                <hr class="footer-line w-44 block text-center">
+            </div>
+
+            <div class="text-black flex justify-center py-2.5">
+                <a href="https://www.instagram.com/bibliotecasocialevillaseta?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
+                    <svg class="w-8 h-8 footer-svg mx-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path fill="currentColor" fill-rule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clip-rule="evenodd"/>
+                    </svg>
+                </a>
+                <a class="flex items-center" href="https://www.youtube.com/@tttactionaidagrigento">
+                    <i class="text-3xl footer-svg fa-brands fa-youtube"></i>
+                </a>
+            </div>
+
+        </div>
+
+        <div class=" w-1/4 flex flex-col items-center justify-center">
+            <div class="w-full flex justify-center pb-2.5">
+                <h3 class="footer-line text-2xl"><strong>Contact</strong></h3>
+            </div>
+
+            <ul class="text-black">
+                <li class="flex pb-1.5">
+                    <svg class="w-7 h-7 footer-svg" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.427 14.768 17.2 13.542a1.733 1.733 0 0 0-2.45 0l-.613.613a1.732 1.732 0 0 1-2.45 0l-1.838-1.84a1.735 1.735 0 0 1 0-2.452l.612-.613a1.735 1.735 0 0 0 0-2.452L9.237 5.572a1.6 1.6 0 0 0-2.45 0c-3.223 3.2-1.702 6.896 1.519 10.117 3.22 3.221 6.914 4.745 10.12 1.535a1.601 1.601 0 0 0 0-2.456Z"/>
+                    </svg>
+
+
+                    <div class="text-sm flex items-center ps-5">
+                        <p>+39 347 123 4569</p>
+                    </div>
+                </li>
+                <li class="flex">
+                    <svg class="w-7 h-7 footer-svg" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m3.5 5.5 7.893 6.036a1 1 0 0 0 1.214 0L20.5 5.5M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z"/>
+                    </svg>
+
+
+                    <div class="text-sm flex items-center ps-5">
+                        <p>bibliotecavillaseta@gmail.com</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
     </footer>
 </template>
 
@@ -11,5 +98,13 @@
 </script>
 
 <style scoped>
+
+    .footer-line {
+        color: #E6544C;
+    }
+
+    .footer-svg {
+        color: #E6544C;
+    }
 
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import "./style.css";
 import "flowbite";
+import "@fortawesome/fontawesome-free";
 import App from "./App.vue";
 import { router } from "./router";
 import axios from "axios";
