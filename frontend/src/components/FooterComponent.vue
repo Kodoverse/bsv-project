4001771da9b0f484aec6761b1b2875f4efa97f53
mattia_morriale <mattia.morriale1@gmail.com>
2025-04-29 16:04:18 +0200

    manual fix

--- frontend/src/components/FooterComponent.vue
+++ frontend/src/components/FooterComponent.vue
@@ -47,12 +47,12 @@
             </div>
 
             <div class="text-black flex justify-center py-2.5">
-                <a href="https://www.instagram.com/bibliotecasocialevillaseta?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
-                    <svg class="w-8 h-8 footer-svg mx-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <a class="mx-1.5" href="https://www.instagram.com/bibliotecasocialevillaseta?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
+                    <svg class="w-8 h-8 footer-svg " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path fill="currentColor" fill-rule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clip-rule="evenodd"/>
                     </svg>
                 </a>
-                <a class="flex items-center" href="https://www.youtube.com/@tttactionaidagrigento">
+                <a class="flex items-center mx-1.5" href="https://www.youtube.com/@tttactionaidagrigento">
                     <i class="text-3xl footer-svg fa-brands fa-youtube"></i>
                 </a>
             </div>
