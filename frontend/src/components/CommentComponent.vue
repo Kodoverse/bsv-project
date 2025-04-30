9da918664800c25018e9f2fc1149fbd62dce91d7
AngeloC89 <Angelociulla89@gmail.com>
2025-04-30 18:41:55 +0200

    created comment component

---
+++ frontend/src/components/CommentComponent.vue
@@ -0,0 +1,89 @@
+<template>
+    <section class="bg-white dark:bg-gray-900 py-8 lg:py-16 antialiased">
+        <div class="max-w-2xl mx-auto px-4">
+            <div class="flex justify-between items-center mb-6">
+                <h2 class="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">Discussion (20)</h2>
+            </div>
+            <form class="mb-6">
+                <div
+                    class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+                    <label for="comment" class="sr-only">Your comment</label>
+                    <textarea id="comment" rows="6"
+                        class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
+                        placeholder="Write a comment..." required></textarea>
+                </div>
+                <button type="submit"
+                    class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+                    Post comment
+                </button>
+            </form>
+
+            <article class="p-6 text-base bg-white rounded-lg dark:bg-gray-900">
+                <footer class="flex justify-between items-center mb-2">
+                    <div class="flex items-center">
+                        <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
+                            <img class="mr-2 w-6 h-6 rounded-full"
+                                src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
+                                alt="Michael Gough">Michael Gough
+                        </p>
+                        <p class="text-sm text-gray-600 dark:text-gray-400"><time pubdate datetime="2022-02-08"
+                                title="February 8th, 2022">Feb. 8, 2022</time></p>
+                    </div>
+                </footer>
+                <p class="text-gray-500 dark:text-gray-400">Very straight-to-point article. Really worth time reading.
+                    Thank you! But tools are just the
+                    instruments for the UX designers. The knowledge of the design tools are as important as the
+                    creation of the design strategy.</p>
+                <div class="flex items-center justify-between mt-4 space-x-4 ">
+                    <div class="flex items-center space-x-4">
+                        <button type="button"
+                            class="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400 font-medium">
+                            <svg class="mr-1.5 w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="none" viewBox="0 0 20 18">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z" />
+                            </svg>
+                            Reply
+                        </button>
+
+                        <button type="button"
+                            class="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400 font-medium">
+                            <svg class="mr-1.5 w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="none" viewBox="0 0 20 18">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z" />
+                            </svg>
+                            Like
+                        </button>
+                    </div>
+
+                    <div>
+                        <button type="button"
+                            class="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400 font-medium">
+                            <svg class="mr-1.5 w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="none" viewBox="0 0 20 18">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z" />
+                            </svg>
+                            Flag
+                        </button>
+                    </div>
+
+
+                </div>
+            </article>
+
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "CommentComponent"
+    }
+</script>
+
+<style lang="scss" scoped></style>
---
+++ frontend/src/pages/SingleArticle.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="flex flex-col">
+        <div id="single-article">
+            <div>
+                <h1 class="text-3xl text-center ">Titolo</h1>
+            </div>
+
+            <div class="my-3">
+                <h2 class="text-2xl text-center ">
+                    Sottotitolo
+                </h2>
+            </div>
+
+            <div>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab, laborum. Iure dolorum incidunt ad dolorem quis eligendi, deserunt laborum commodi alias veritatis magni saepe error cum reprehenderit pariatur eaque vitae corrupti beatae doloribus quidem nostrum animi et! Tempora dolores architecto praesentium obcaecati molestiae nisi adipisci? Dolor voluptate delectus pariatur porro quasi, at dolores assumenda facilis iure provident natus mollitia minima, soluta fugit culpa aliquam nam esse facere quod alias doloremque amet non nisi! Ipsum eaque, earum provident est placeat tempore consequuntur minima? Rem recusandae iusto qui cum hic debitis necessitatibus consectetur, tenetur error obcaecati molestias nesciunt unde. Ea quisquam impedit exercitationem quidem architecto sed id nam doloremque perspiciatis beatae obcaecati odit possimus maxime, quos excepturi non nobis hic tenetur voluptate. Quo at labore quod quas alias reiciendis veniam repellat? Odio, adipisci mollitia provident nam nulla, culpa velit, alias libero eligendi necessitatibus a. Perspiciatis dolorem dicta nemo quod doloremque illo, maxime soluta vel pariatur sequi ab aspernatur natus alias mollitia tenetur nulla quaerat, sit harum. Quos tempore temporibus est, ex doloribus cumque voluptatibus exercitationem laboriosam maxime nihil nulla quidem id, fugiat nam neque cupiditate eaque dolor debitis pariatur, odio sed maiores hic quas. Consequuntur quis asperiores quidem veniam non, repellendus ea, necessitatibus est deleniti dolores in pariatur? Possimus magni id veritatis similique temporibus cum, fugiat earum delectus atque alias praesentium corrupti quas saepe aspernatur sit tenetur doloribus quod iure nobis necessitatibus ex, facere, repellat asperiores quos? Amet architecto eaque tenetur expedita non accusamus, corrupti qui sint! Enim molestias iure quia deleniti vero aspernatur nam sequi, assumenda perferendis ipsum. Alias, sapiente. Eum, excepturi. Similique quis doloribus placeat ullam tempore repellendus eum minima esse? Illum, aperiam. Maxime itaque illo ratione sed quos nobis aliquid, et animi eaque cupiditate unde in iure vel adipisci voluptatem iste. Nam, voluptatum optio! Assumenda numquam fugiat asperiores praesentium?
+                </p>
+            </div>
+        </div>
+
+    <
+    <div id="comments">
+        <CommentComponent />
+    </div>
+    </div>
+  
+</template>
+
+<script>
+    import CommentComponent from "../components/CommentComponent.vue";
+    import axios from "axios";
+    export default {
+        name: "SingleArticle",
+        components: {
+            CommentComponent,
+        },
+        data() {
+            return {
+
+            }
+
+        },
+        methods: {
+            
+
+        }
+    }
+</script>
+
+<style scoped>
+    #single-article {
+        height: 100%;
+        width: 100%;
+        border: 3px solid red;
+    }
+
+    #comments {
+        height: 100%;
+        width: 100%;
+        border: 3px solid green;
+    }
+</style>
