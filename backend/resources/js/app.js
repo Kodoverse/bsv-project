b060b8e790186c8b395ea7646c6facfba3e786f7
Emanuele Pogliari <manu.pogliari@gmail.com>
2025-09-23 10:45:49 +0200

    configurazione vue e primo test

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -3,5 +3,8 @@ import "flowbite";
 import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
+import { createApp } from "vue";
+import Dashboard from "./components/Dashboard.vue";
 
+createApp(Dashboard).mount("#test-vue");
 Alpine.start();
--- backend/resources/views/dashboard.blade.php
+++ backend/resources/views/dashboard.blade.php
@@ -3,6 +3,7 @@
 
     <x-app-layout>
 
+        <div id="test-vue"></div>
 
         <div class="flex flex-col items-center w-full overflow-hidden">
 
@@ -10,13 +11,13 @@
                 <div class="w-full">
                     <div>
                         <div class="flex justify-between">
-                            <div v-if="user" class="flex flex-col gap-5 ps-6 py-6">
+                            <div v-if="user" class="flex flex-col gap-5 py-6 ps-6">
                                 <h1 class="text-3xl tracking-wide uppercase">My dashboard</h1>
                             </div>
                         </div>
 
-                        <div class="px-6 w-full">
-                            <div class="mx-auto  grid grid-cols-4 gap-6">
+                        <div class="w-full px-6">
+                            <div class="grid grid-cols-4 gap-6 mx-auto">
                                 <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                                     <div class="p-6 text-gray-900 dark:text-gray-100">
                                         {{ __("Notifiche") }}
@@ -43,10 +44,10 @@
                             </div>
                         </div>
 
-                        <div class="p-6 w-full">
+                        <div class="w-full p-6">
                             <div class="grid grid-cols-1 gap-6 lg:grid-cols-2">
                                 <div class="grid grid-cols-1 lg:grid-cols-1">
-                                    <div class="p-6 rounded-md my-shadow bg-gray-800">
+                                    <div class="p-6 bg-gray-800 rounded-md my-shadow">
                                         <h2 class="mb-5 text-2xl uppercase">Your Credentials</h2>
                                       
                                             <div>
@@ -58,12 +59,12 @@
                                     </div>
                                 </div>
                                 <div class="grid grid-cols-1 gap-6 lg:grid-cols-1">
-                                    <div class="p-6 rounded-md my-shadow bg-gray-800">
+                                    <div class="p-6 bg-gray-800 rounded-md my-shadow">
                                         <div class="flex items-start justify-between mb-4">
                                             <h2 class="mb-5 text-2xl">ORDERS</h2>
                                         </div>
                                     </div>
-                                    <div class="p-6 rounded-md my-shadow bg-gray-800">
+                                    <div class="p-6 bg-gray-800 rounded-md my-shadow">
                                         <div class="flex items-start justify-between mb-4">
                                             <h2 class="mb-5 text-2xl uppercase">Your Consultations</h2>
                                         </div>
