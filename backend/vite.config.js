e48bc723342cf6594b2aded95e88aa7fc66f9aa1
Emanuele Pogliari <manu.pogliari@gmail.com>
2025-10-13 12:36:46 +0200

    configurazione vite

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -1,18 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import laravel from "laravel-vite-plugin";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
-            refresh: true,
-        }),
-    ],
-    server: {
-        host: "0.0.0.0",
-        port: 5174,
-        hmr: {
-            host: "192.168.0.115",
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return {
+        server: {
+            host: env.VITE_HOST || "0.0.0.0",
+            port: Number(env.VITE_PORT) || 5174,
+            strictPort: true,
+            hmr: {
+                host: env.VITE_HMR_HOST || "localhost",
+                port: Number(env.VITE_PORT) || 5174,
+                protocol: "ws",
+            },
         },
-    },
+        plugins: [
+            laravel({
+                input: ["resources/css/app.css", "resources/js/app.js"],
+                refresh: true,
+            }),
+        ],
+    };
 });
